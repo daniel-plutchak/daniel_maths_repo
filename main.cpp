1ccd894b57631e777db2c01f97ec149dc3432ce9
daniel-plutchak <dplutchak@carthage.edu>
2025-03-06 13:15:44 -0600

    Added square function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 using namespace std;
 
+void square(int val);
 
 int main(){
     cout << "GitHub Test" << endl;
+    square(5);
     return 0;
+}
+
+void square(int val) {
+    cout << val * val << endl;
 }
