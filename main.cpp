a47553f96c94e60d9a34268d64fe7061aa3b1396
daniel-plutchak <dplutchak@carthage.edu>
2025-03-06 13:18:02 -0600

    Changed square to power

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 using namespace std;
 
-void square(int val);
+void power(int base, int exp);
 
 int main(){
     cout << "GitHub Test" << endl;
-    square(5);
+    power(5, 3);
     return 0;
 }
 
-void square(int val) {
-    cout << val * val << endl;
+void power(int base, int exp) {
+    int val = 1;
+    for (int i = 0; i < exp; ++i) {
+        val *= base;
+    }
+    cout << val << endl;
 }
